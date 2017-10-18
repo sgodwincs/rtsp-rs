471f732bc7bc2395df15eabe44ca844e9f58745b
Scott Godwin <sgodwincs@gmail.com>
2017-10-18 19:42:34 -0400

    fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ pub mod method;
 pub mod status;
 pub mod version;
 
-pub use header::HeaderName;
+pub use header::{HeaderName, HeaderValue};
 pub use method::Method;
 pub use status::{StatusCode, StatusCodeClass};
 pub use version::Version;
