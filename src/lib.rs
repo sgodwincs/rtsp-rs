3ad33640c8c55fcc6b51d8f574413a023c12d368
Scott Godwin <sgodwincs@gmail.com>
2017-10-22 03:15:28 -0400

    fix lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(try_from)]
 
 extern crate ascii;
+extern crate fnv;
 
 #[macro_use]
 macro_rules! byte_map {
@@ -14,7 +15,7 @@ pub mod method;
 pub mod status;
 pub mod version;
 
-pub use header::{HeaderName, HeaderValue};
+pub use header::{HeaderMap, HeaderName, HeaderValue};
 pub use method::Method;
 pub use status::{StatusCode, StatusCodeClass};
 pub use version::Version;
