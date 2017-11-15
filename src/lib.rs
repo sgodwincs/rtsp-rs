8d416ad100b8c3473f2390a717e0ae0410ec2298
Scott Godwin <sgodwincs@gmail.com>
2017-11-15 05:51:10 -0500

    add quoted string parsing, move syntax parsing to syntax.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,17 @@ extern crate ascii;
 extern crate bytes;
 extern crate fnv;
 extern crate futures;
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
 extern crate tokio_io;
 extern crate tokio_proto;
 extern crate tokio_service;
 extern crate url;
 
-#[macro_use]
-macro_rules! byte_map {
-    ($($flag:expr,)*) => ([
-        $($flag != 0,)*
-    ])
-}
-
 // pub mod proto;
+
+mod syntax;
 
 pub mod header;
 pub mod method;
@@ -32,47 +30,3 @@ pub use request::Request;
 pub use response::Response;
 pub use status::{StatusCode, StatusCodeClass};
 pub use version::Version;
-
-#[cfg_attr(rustfmt, rustfmt_skip)]
-const TOKEN_CHAR_MAP: [bool; 256] = byte_map![
- // 0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 1
-    0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, // 2
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, // 3
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 4
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, // 5
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 6
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, // 7
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 8
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 9
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // A
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // B
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // C
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // D
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // E
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // F
-];
-
-fn is_token_char(b: u8) -> bool {
-    TOKEN_CHAR_MAP[b as usize]
-}
-
-fn is_token<T>(token: T) -> bool
-where
-    T: AsRef<[u8]>,
-{
-    let bytes = token.as_ref();
-
-    if bytes.is_empty() {
-        return false;
-    }
-
-    for &b in bytes {
-        if !is_token_char(b) {
-            return false;
-        }
-    }
-
-    true
-}
---
+++ src/syntax.rs
@@ -0,0 +1,63 @@
+use regex::Regex;
+
+#[macro_use]
+macro_rules! byte_map {
+    ($($flag:expr,)*) => ([
+        $($flag != 0,)*
+    ])
+}
+
+lazy_static! {
+    static ref QUOTED_STRING_RE: Regex =
+        Regex::new(r#""((?:[ !#-\[\]-~]|[^[:ascii:]]|\\"|\\)*)""#).unwrap();
+}
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
+const TOKEN_CHAR_MAP: [bool; 256] = byte_map![
+ // 0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 1
+    0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, // 2
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, // 3
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 4
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, // 5
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 6
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, // 7
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 8
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 9
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // A
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // B
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // C
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // D
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // E
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // F
+];
+
+pub fn quoted_string(string: &str) -> Option<&str> {
+    QUOTED_STRING_RE
+        .find(string)
+        .map(|m| &string[m.start() + 1..m.end() - 1])
+}
+
+pub fn is_token_char(b: u8) -> bool {
+    TOKEN_CHAR_MAP[b as usize]
+}
+
+pub fn is_token<T>(token: T) -> bool
+where
+    T: AsRef<[u8]>,
+{
+    let bytes = token.as_ref();
+
+    if bytes.is_empty() {
+        return false;
+    }
+
+    for &b in bytes {
+        if !is_token_char(b) {
+            return false;
+        }
+    }
+
+    true
+}
