94aed67d67761f09e561e0dcd34fea5f1118f993
Scott Godwin <sgodwincs@gmail.com>
2017-12-27 12:03:10 -0500

    move helper functions up, remove impossible error types

--- src/proto/server.rs
+++ src/proto/server.rs
@@ -3,12 +3,14 @@ use std::{error, fmt, io};
 use std::mem::replace;
 use tokio_io::codec::{Decoder, Encoder};
 
+use super::{consume_line, trim_header};
 use header::{Entry, HeaderName, HeaderValue, TypedHeader};
 use header::types::ContentLength;
 use request::{Builder, BuilderError, Request};
 use response::Response;
 use version::Version;
 
+#[derive(Debug)]
 pub struct ServerCodec {
     body: Option<BytesMut>,
     builder: Builder,
@@ -16,35 +18,12 @@ pub struct ServerCodec {
     state: ParseState,
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Debug, Eq, Hash, PartialEq)]
 pub enum ParseState {
     Body,
     End,
     Header,
     RequestLine,
-}
-
-/// Consumes a line from the buffer, returning the line found and the index at which the `\r\n`
-/// started.
-fn consume_line(buffer: &mut BytesMut) -> Option<(BytesMut, usize)> {
-    if let Some(i) = buffer.windows(2).position(|b| b == b"\r\n") {
-        let line = buffer.split_to(i);
-        buffer.split_to(2);
-        Some((line, i))
-    } else {
-        None
-    }
-}
-
-/// Trims the `&[u8]` of any trailing spaces or tabs. It is possible that the header is actually
-/// UTF-8 encoded and this function will create an invalid value, but since header names have to be
-/// ASCII-US encoded, it should not cause any problems.
-fn trim_header(mut header: &[u8]) -> &[u8] {
-    while header.ends_with(&[b' ']) || header.ends_with(&[b'\t']) {
-        header = &header[0..header.len() - 1];
-    }
-
-    header
 }
 
 impl ServerCodec {
@@ -292,8 +271,6 @@ pub enum InvalidRequest {
     InvalidMethod,
     InvalidURI,
     InvalidVersion,
-    MissingMethod,
-    MissingURI,
 }
 
 impl fmt::Display for InvalidRequest {
@@ -314,8 +291,6 @@ impl error::Error for InvalidRequest {
             &InvalidMethod => "invalid RTSP request-  invalid method",
             &InvalidURI => "invalid RTSP request - invalid URI",
             &InvalidVersion => "invalid RTSP request - invalid version",
-            &MissingMethod => "invalid RTSP request - missing method",
-            &MissingURI => "invalid RTSP request - missing URI",
         }
     }
 }
@@ -330,8 +305,7 @@ impl From<BuilderError> for InvalidRequest {
             BuilderError::InvalidMethod => InvalidMethod,
             BuilderError::InvalidURI => InvalidURI,
             BuilderError::InvalidVersion => InvalidVersion,
-            BuilderError::MissingMethod => MissingMethod,
-            BuilderError::MissingURI => MissingURI,
+            _ => panic!("rest of `BuilderError`s should not be accessible"),
         }
     }
 }
