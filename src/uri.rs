6c88ce23c5f6a30f2a51b05599bce021036aa301
Scott Godwin <sgodwincs@gmail.com>
2018-01-01 18:36:25 -0500

    refactor uri.rs

--- src/uri.rs
+++ src/uri.rs
@@ -1,27 +1,14 @@
 //! RTSP URI
 //!
 //! This is a wrapper around the `Url` type from the `url` crate. This is done because request URIs
-//! do not have to actually be URIs (can be `'*'`) and providing `TryFrom` implementations for this
-//! type make it easier to deal with the `Request` type. Also, the `Url` type is a strict superset
+//! do not have to actually be URIs (can be `"*"`) and providing `TryFrom` implementations for this
+//! type makes it easier to deal with the `Request` type. Also, the `Url` type is a strict superset
 //! of valid URIs that are allowed in the request line, specifically, the URIs must contain an
 //! authority part.
 
-use std::convert::TryFrom;
 use std::{error, fmt, str};
+use std::convert::TryFrom;
 use url::Url;
-
-/// This is a wrapper type to prevent users from creating instances of `URI` with unchecked `Url`s.
-/// This unfortunately makes using `URI` slightly inconvenient as another level of redirection is
-/// required to access the underlying `Url`.
-#[derive(Clone, Eq, Hash, PartialEq)]
-pub struct URIInner(Url);
-
-impl URIInner {
-    /// Retrieves the underlying `Url` type immutably.
-    pub fn uri(&self) -> &Url {
-        &self.0
-    }
-}
 
 /// The main type of this module. Represents either a valid URI or a URI of the form `'*'`.
 #[derive(Clone, Eq, Hash, PartialEq)]
@@ -39,8 +26,8 @@ impl URI {
     /// ```
     /// # #![feature(try_from)]
     /// #
-    /// # use std::convert::TryFrom;
-    /// #
+    /// use std::convert::TryFrom;
+    ///
     /// use rtsp::URI;
     ///
     /// let uri = URI::try_from("rtsp://example.com").unwrap();
@@ -57,29 +44,68 @@ impl URI {
             URI(ref uri) => uri.0.as_str(),
         }
     }
-}
 
-impl fmt::Debug for URIInner {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self.0)
+
+    /// Returns whether or not the uri is `Any`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #![feature(try_from)]
+    /// #
+    /// use std::convert::TryFrom;
+    ///
+    /// use rtsp::URI;
+    ///
+    /// assert!(URI::Any.is_any());
+    ///
+    /// let uri = URI::try_from("rtsp://example.com").unwrap();
+    /// assert!(!uri.is_any());
+    /// ```
+    pub fn is_any(&self) -> bool {
+        use self::URI::*;
+
+        match *self {
+            Any => true,
+            _ => false,
+        }
     }
-}
 
-impl fmt::Display for URIInner {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.0)
+    /// Returns whether or not the uri is `URI`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #![feature(try_from)]
+    /// #
+    /// # use std::convert::TryFrom;
+    /// #
+    /// use rtsp::URI;
+    ///
+    /// assert!(!URI::Any.is_uri());
+    ///
+    /// let uri = URI::try_from("rtsp://example.com").unwrap();
+    /// assert!(uri.is_uri());
+    /// ```
+    pub fn is_uri(&self) -> bool {
+        use self::URI::*;
+
+        match *self {
+            URI(_) => true,
+            _ => false,
+        }
     }
 }
 
 impl fmt::Debug for URI {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self.as_str())
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str(self.as_str())
     }
 }
 
 impl fmt::Display for URI {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.as_str())
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str(self.as_str())
     }
 }
 
@@ -96,38 +122,64 @@ impl fmt::Display for URI {
 impl<'a> TryFrom<&'a [u8]> for URI {
     type Error = InvalidURI;
 
-    /// Converts a `&[u8]` to an RTSP URI. The URI must be either a valid URI or must be `'*'`.
+    /// Converts a `&[u8]` to an RTSP URI. The URI must be either a valid URI with an authority part
+    /// or `"*"`.
     ///
     /// # Examples
     ///
     /// ```
     /// # #![feature(try_from)]
     /// #
-    /// # use std::convert::TryFrom;
-    /// #
+    /// use std::convert::TryFrom;
+    ///
     /// use rtsp::URI;
     ///
     /// let any = URI::try_from(&b"*"[..]).unwrap();
     /// assert_eq!(any, URI::Any);
     ///
     /// let uri = URI::try_from(&b"rtsp://example.com"[..]).unwrap();
-    ///
-    /// match uri {
-    ///     URI::Any => panic!("expected valid URI"),
-    ///     URI::URI(uri) => println!("got uri: {}", uri)
-    /// }
+    /// assert!(uri.is_uri());
     ///
     /// let error = URI::try_from(&b"this is an invalid URI"[..]);
     /// assert!(error.is_err());
     /// ```
     fn try_from(value: &'a [u8]) -> Result<Self, Self::Error> {
+        let value = str::from_utf8(value).map_err(|_| InvalidURI)?;
+        URI::try_from(value)
+    }
+}
+
+impl<'a> TryFrom<&'a str> for URI {
+    type Error = InvalidURI;
+
+    /// Converts a `&str` to an RTSP URI. The URI must be either a valid URI with an authority part
+    /// or `"*"`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #![feature(try_from)]
+    /// #
+    /// use std::convert::TryFrom;
+    ///
+    /// use rtsp::URI;
+    ///
+    /// let any = URI::try_from(&b"*"[..]).unwrap();
+    /// assert_eq!(any, URI::Any);
+    ///
+    /// let uri = URI::try_from(&b"rtsp://example.com"[..]).unwrap();
+    /// assert!(uri.is_uri());
+    ///
+    /// let error = URI::try_from(&b"this is an invalid URI"[..]);
+    /// assert!(error.is_err());
+    /// ```
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
         use self::URI::*;
 
-        if value.len() == 1 && value[0] == b'*' {
+        if value.len() == 1 && value == "*" {
             Ok(Any)
         } else {
-            let uri = str::from_utf8(value).map_err(|_| InvalidURI)?;
-            let uri = Url::parse(uri).map_err(|_| InvalidURI)?;
+            let uri = Url::parse(value).map_err(|_| InvalidURI)?;
 
             if uri.has_authority() {
                 Ok(URI(URIInner(uri)))
@@ -138,11 +190,28 @@ impl<'a> TryFrom<&'a [u8]> for URI {
     }
 }
 
-impl<'a> TryFrom<&'a str> for URI {
-    type Error = InvalidURI;
+/// This is a wrapper type to prevent users from creating instances of `URI` with unchecked `Url`s.
+/// This unfortunately makes using `URI` slightly inconvenient as another level of redirection is
+/// required to access the underlying `Url`.
+#[derive(Clone, Eq, Hash, PartialEq)]
+pub struct URIInner(Url);
 
-    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
-        URI::try_from(value.as_bytes())
+impl URIInner {
+    /// Retrieves the underlying `Url` type immutably.
+    pub fn uri(&self) -> &Url {
+        &self.0
+    }
+}
+
+impl fmt::Debug for URIInner {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.0)
+    }
+}
+
+impl fmt::Display for URIInner {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
 
@@ -151,10 +220,10 @@ impl<'a> TryFrom<&'a str> for URI {
 pub struct InvalidURI;
 
 impl fmt::Display for InvalidURI {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         use std::error::Error;
 
-        write!(f, "{}", self.description())
+        formatter.write_str(self.description())
     }
 }
 
