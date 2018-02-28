875c35b29943064d6fb3d85564e39773fb7e6fdc
Scott Godwin <sgodwincs@gmail.com>
2018-02-28 17:56:57 -0500

    export header constants

--- src/header/types/mod.rs
+++ src/header/types/mod.rs
@@ -2,6 +2,6 @@ mod content_length;
 mod cseq;
 mod session;
 
-pub use self::content_length::ContentLength;
-pub use self::cseq::CSeq;
+pub use self::content_length::{ContentLength, MAX_CONTENT_LENGTH};
+pub use self::cseq::{CSeq, MAX_CSEQ};
 pub use self::session::Session;
