65decea2f9e4920a5d7263884304498b04738909
Sarah Allen <sarah@ultrasaurus.com>
2019-07-09 15:29:48 -0700

    extend example to allow user to specify URI
    Co-authored-by: Dennis Collinson <dennis.collective@gmail.com>

--- examples/client.rs
+++ examples/client.rs
@@ -9,11 +9,35 @@ use rtsp::client::Client;
 use rtsp::method::Method;
 use rtsp::request::Request;
 use rtsp::uri::request::URI;
+use rtsp::uri::Host;
+use rtsp::uri::RTSP_DEFAULT_PORT;
 use std::convert::TryFrom;
+use std::net::IpAddr;
 use std::net::SocketAddr;
 
 fn main() {
-    let address = "127.0.0.1:10500".parse::<SocketAddr>().unwrap();
+    let uri_string = std::env::args()
+        .nth(1)
+        .unwrap_or(String::from("rtsp://127.0.0.1:10500"));
+
+    let uri = URI::try_from(uri_string.as_str()).expect("Invalid URI");
+
+    let host = uri.host().unwrap();
+    let addr: IpAddr = match host {
+        Host::IPv4Address(ip) => IpAddr::V4(*ip),
+        Host::IPv6Address(ip) => IpAddr::V6(*ip),
+        _ => {
+            eprintln!(
+                "Please provide ip address. Hostname not supported: {}",
+                host
+            );
+            std::process::exit(1);
+        }
+    };
+
+    let address = SocketAddr::new(addr, uri.port().unwrap_or(RTSP_DEFAULT_PORT));
+
+    println!("Initiating connection to: {}", address);
 
     // Connect to the server. Currently, any requests sent by the server will be ignored by the
     // client. An API to support handling these requests will be added soonish.
