a7af459edd7bc4863e8fc7cc6837108266a99ec4
Govind <venkateshgovind1@gmail.com>
2019-07-18 23:55:09 +0200

    removed unused imports

--- src/header/types/accept.rs
+++ src/header/types/accept.rs
@@ -12,76 +12,11 @@ use linked_hash_set::LinkedHashSet;
 use crate::header::value::HeaderValue;
 use crate::header::name::HeaderName;
 use crate::syntax;
-use syntax::is_token;
-use std::fmt::{self, Display, Formatter};
 use std::string::ToString;
 use itertools::Itertools;
 use mime::*;
 
 
-
-
-/// The Accept request-header field can be used to specify certain
-/// presentation description and parameter media types [RFC6838] that are
-/// acceptable for the response to the DESCRIBE request.
-/// 
-/// ```text
-/// Accept            =  "Accept" HCOLON
-///                        [ accept-range *(COMMA accept-range) ]
-///    accept-range      =  media-type-range [SEMI accept-params]
-///    accept-params     =  "q" EQUAL qvalue *(SEMI generic-param )
-///    qvalue            =  ( "0" [ "." *3DIGIT ] )
-///                      /  ( "1" [ "." *3("0") ] )
-///    generic-param   =  token [ EQUAL gen-value ] //no example usage so not included
-///    gen-value       =  token / host / quoted-string
-///    host              = < As defined in RFC 3986>
-///
-///    media-type-range  =  ( "*//*"
-///                            / ( m-type SLASH "*" )
-///                            / ( m-type SLASH m-subtype )
-///                                ) *( SEMI m-parameter )
-///    m-type             =  discrete-type / composite-type
-///    discrete-type      =  "text" / "image" / "audio" / "video"
-///                      /  "application" / extension-token
-///    composite-type   =  "message" / "multipart" / extension-token
-///    extension-token  =  ietf-token / x-token
-///    ietf-token       =  token
-///    x-token          =  "x-" token
-///    m-subtype        =  extension-token / iana-token
-///    iana-token       =  token
-///    m-parameter      =  m-attribute EQUAL m-value
-///    m-attribute      =  token
-///    m-value          =  token / quoted-string
-///    token           =  1*(%x21 / %x23-27 / %x2A-2B / %x2D-2E / %x30-39
-///                        /  %x41-5A / %x5E-7A / %x7C / %x7E) //any CHAR except CTLs or tspecials
-/// ```
-/// 
-/// All values seperated with commas will be converted into [`MediaType`] type.
-/// 
-/// # Examples
-/// 
-/// ```
-/// use std::convert::TryFrom;
-///
-/// use rtsp::header::map::TypedHeader;
-/// use rtsp::header::types::accept_ranges::RangeFormat;
-/// use rtsp::header::types::Accept;
-/// use rtsp::header::types::accept::*;
-/// use rtsp::header::value::HeaderValue;
-/// use mime::*;
-/// use std::str::FromStr;
-/// let raw_header: Vec<HeaderValue> = vec![];
-/// assert_eq!(Accept::decode(&mut raw_header.iter()).unwrap(), None);
-///
-/// let typed_header = vec![MediaType::new(mime::STAR_STAR, None), MediaType::new(Mime::from_str("video/mp4").unwrap(), Some(QualityParam::new(0.5)))]
-///     .into_iter()
-///     .collect::<Accept>();
-/// let raw_header = vec![HeaderValue::try_from("*/*, video/mp4;q=0.5").unwrap()];
-/// assert_eq!(
-///     Accept::decode(&mut raw_header.iter()).unwrap(),
-///     Some(typed_header)
-/// ); 
-/// ```
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct Accept(LinkedHashSet<MediaType>);/// use rtsp::header::types::accept::*;
 
@@ -115,6 +50,67 @@ impl FromIterator<MediaType> for Accept {
 impl TypedHeader for Accept {
     type DecodeError = AcceptError;
 
+    /// The Accept request-header field can be used to specify certain
+    /// presentation description and parameter media types [RFC6838] that are
+    /// acceptable for the response to the DESCRIBE request.
+    /// 
+    /// ```text
+    /// Accept            =  "Accept" HCOLON
+    ///                        [ accept-range *(COMMA accept-range) ]
+    ///    accept-range      =  media-type-range [SEMI accept-params]
+    ///    accept-params     =  "q" EQUAL qvalue *(SEMI generic-param )
+    ///    qvalue            =  ( "0" [ "." *3DIGIT ] )
+    ///                      /  ( "1" [ "." *3("0") ] )
+    ///    generic-param   =  token [ EQUAL gen-value ] //no example usage so not included
+    ///    gen-value       =  token / host / quoted-string
+    ///    host              = < As defined in RFC 3986>
+    ///
+    ///    media-type-range  =  ( "*//*"
+    ///                            / ( m-type SLASH "*" )
+    ///                            / ( m-type SLASH m-subtype )
+    ///                                ) *( SEMI m-parameter )
+    ///    m-type             =  discrete-type / composite-type
+    ///    discrete-type      =  "text" / "image" / "audio" / "video"
+    ///                      /  "application" / extension-token
+    ///    composite-type   =  "message" / "multipart" / extension-token
+    ///    extension-token  =  ietf-token / x-token
+    ///    ietf-token       =  token
+    ///    x-token          =  "x-" token
+    ///    m-subtype        =  extension-token / iana-token
+    ///    iana-token       =  token
+    ///    m-parameter      =  m-attribute EQUAL m-value
+    ///    m-attribute      =  token
+    ///    m-value          =  token / quoted-string
+    ///    token           =  1*(%x21 / %x23-27 / %x2A-2B / %x2D-2E / %x30-39
+    ///                        /  %x41-5A / %x5E-7A / %x7C / %x7E) //any CHAR except CTLs or tspecials
+    /// ```
+    /// 
+    /// All values seperated with commas will be converted into [`MediaType`] type.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use std::convert::TryFrom;
+    ///
+    /// use rtsp::header::map::TypedHeader;
+    /// use rtsp::header::types::accept_ranges::RangeFormat;
+    /// use rtsp::header::types::Accept;
+    /// use rtsp::header::types::accept::*;
+    /// use rtsp::header::value::HeaderValue;
+    /// use mime::*;
+    /// use std::str::FromStr;
+    /// let raw_header: Vec<HeaderValue> = vec![];
+    /// assert_eq!(Accept::decode(&mut raw_header.iter()).unwrap(), None);
+    ///
+    /// let typed_header = vec![MediaType::new(mime::STAR_STAR, None), MediaType::new(Mime::from_str("video/mp4").unwrap(), Some(QualityParam::new(0.5)))]
+    ///     .into_iter()
+    ///     .collect::<Accept>();
+    /// let raw_header = vec![HeaderValue::try_from("*/*, video/mp4;q=0.5").unwrap()];
+    /// assert_eq!(
+    ///     Accept::decode(&mut raw_header.iter()).unwrap(),
+    ///     Some(typed_header)
+    /// ); 
+    /// ```
     fn decode<'header, Iter>(values: &mut Iter) -> Result<Option<Self>, Self::DecodeError>
     where
         Iter: Iterator<Item = &'header HeaderValue>,
@@ -124,8 +120,8 @@ impl TypedHeader for Accept {
         for value in values {
             let media = value.as_str().split(',');
             for mtype in media {
-                let t = MediaType::try_from(syntax::trim_whitespace(mtype))?;
-                accept.insert(t);
+                let media_type = MediaType::try_from(syntax::trim_whitespace(mtype))?;
+                accept.insert(media_type);
             }
             present = true;
         }
@@ -182,21 +178,12 @@ impl TypedHeader for Accept {
     }
 }
 
-/*
-    Each media type or range MAY be followed by one or more accept-
-    params, beginning with the "q" parameter to indicate a relative
-    quality factor.  The first "q" parameter (if any) separates the media
-    type or range's parameters from the accept-params.  Quality factors
-    allow the user or user agent to indicate the relative degree of
-    preference for that media type, using the qvalue scale from 0 to 1
-    (Section 5.3.1 of [RFC7231]).  The default value is q=1.
-*/
 #[derive(Clone, Debug, PartialEq)]
 pub struct MediaType{
     m_type: Mime,
     quality: Option<QualityParam>
 
-} //mtype and msubtype followed by what may be a quality. Then the quality may be followed by a generic param....
+}
 
 impl Default for MediaType {
 
@@ -244,7 +231,7 @@ impl MediaType{
     pub fn as_str(&self) -> String {
         let m_type = self.m_type.to_string();
         match &self.quality {
-            Some(q) => format!("{} ;{}", m_type, q.as_str()),
+            Some(quality) => format!("{} ;{}", m_type, quality.as_str()),
             None => format!("{}", m_type)
         }
     }
@@ -257,11 +244,16 @@ impl<'accept> TryFrom<&'accept [u8]> for MediaType {
         let mut split = value.split(|element| element.clone() == ';' as u8);
         let mut decoded = MediaType::default();
         if let Some(mediatype) = split.next() {
-            let mtype: Mime = str::from_utf8(mediatype).unwrap().parse().unwrap();
-            decoded.m_type = mtype;
-            if let Some(quality) = split.next() {
-                let q = QualityParam::try_from(quality).unwrap();
-                decoded.quality = Some(q);
+            if let Ok(mtype) = str::from_utf8(mediatype).unwrap().parse() {
+                decoded.m_type = mtype;
+                if let Some(quality) = split.next() {
+                    let quality_param = QualityParam::try_from(quality).unwrap();
+                    decoded.quality = Some(quality_param);
+                } else{
+                    return Err(AcceptError("unable to parse quality parameter"))
+                }
+            } else{
+                return Err(AcceptError("unable to parse mime type"))
             }
         }
         Ok(decoded)
@@ -291,7 +283,7 @@ impl<'quality> TryFrom<&'quality [u8]> for QualityParam {
         }        
         let quality = val[1].parse::<f32>();
         match quality {
-            Ok(q) =>  return Ok(QualityParam::new(q)),
+            Ok(quality_param) =>  return Ok(QualityParam::new(quality_param)),
             Err(_) => return Err(AcceptError("unable to parse f32 from string"))
         }
     }
@@ -330,5 +322,3 @@ impl QualityParam {
         format!("q={}", q_val)
     }
 }
-
-pub struct MediaTypeParseError(String);
